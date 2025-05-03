eccb448328590295bdd704990448988bd2c21bdf
Jannatul <limajannat1675@gmail.com>
2025-05-03 22:46:02 +0600

    added code

---
+++ Lecture-1/CP.cpp
@@ -0,0 +1,39 @@
+//Problem-3
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X, Y;
+
+    cin >> X >> Y;
+    cout << X << " + " << Y << " = " << X + Y << endl;
+    
+    cin >> X >> Y;
+    cout << X << " * " << Y << " = " << X * Y << endl;
+        
+    cin >> X >> Y;    
+    cout << X << " - " << Y << " = " << X - Y << endl;
+
+    return 0;
+}
+
+// advanced version
+#include <iostream>
+
+using namespace std;
+int main() {
+    long long X, Y;
+
+    for(int i=1; i<=1; i++) {
+            cin >> X >> Y;  
+    cout << X << " + " << Y << " = " << X + Y << endl;
+
+    cin >> X >> Y;
+    cout << X << " * " << Y << " = " << X * Y << endl;
+
+    cin >> X >> Y;
+    cout << X << " - " << Y << " = " << X - Y << endl;
+    }
+
+    return 0;
+}
